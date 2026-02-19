b293be54cc9c6dcd779bd975dcf35ba65316188f
sowahm808 <sowahm808@gmail.com>
2026-02-19 19:23:16 +0000

    Configure Firebase app with provided project credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api.example.com'
+  apiBaseUrl: 'https://api.example.com',
+  firebase: {
+    apiKey: 'AIzaSyB0ZSfztmOD_-aGVYnRKq2U9keEjCR20To',
+    authDomain: 'ebanfx-db4fb.firebaseapp.com',
+    projectId: 'ebanfx-db4fb',
+    storageBucket: 'ebanfx-db4fb.firebasestorage.app',
+    messagingSenderId: '1091725481207',
+    appId: '1:1091725481207:web:4326197a4b78143262eff2',
+    measurementId: 'G-3V3QCNWN1Z'
+  }
 };
