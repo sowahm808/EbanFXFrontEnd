8912c75dddbe00119df887c155b2e4faea8e8130
sowahm808 <sowahm808@gmail.com>
2026-02-20 07:54:07 +0000

    Fix dashboard focus trap from hidden tab pages

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,19 +1,20 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/angular/standalone';
 import { UserService } from '../../core/services/user.service';
 
 @Component({
   standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonLabel, IonRouterOutlet],
+  imports: [IonTabs, IonTabBar, IonTabButton, IonLabel, IonRouterOutlet, RouterLink],
   template: `
   <ion-tabs>
-    <ion-router-outlet />
+    <ion-router-outlet></ion-router-outlet>
     <ion-tab-bar slot="bottom">
-      <ion-tab-button tab="dashboard" href="/tabs/dashboard"><ion-label>Dashboard</ion-label></ion-tab-button>
-      <ion-tab-button tab="beneficiaries" href="/tabs/beneficiaries"><ion-label>Beneficiaries</ion-label></ion-tab-button>
-      <ion-tab-button tab="orders" href="/tabs/orders"><ion-label>Orders</ion-label></ion-tab-button>
-      <ion-tab-button tab="profile" href="/tabs/profile"><ion-label>Profile</ion-label></ion-tab-button>
-      @if(showAdmin){<ion-tab-button tab="admin" href="/tabs/admin"><ion-label>Admin</ion-label></ion-tab-button>}
+      <ion-tab-button tab="dashboard" [routerLink]="['/tabs/dashboard']"><ion-label>Dashboard</ion-label></ion-tab-button>
+      <ion-tab-button tab="beneficiaries" [routerLink]="['/tabs/beneficiaries']"><ion-label>Beneficiaries</ion-label></ion-tab-button>
+      <ion-tab-button tab="orders" [routerLink]="['/tabs/orders']"><ion-label>Orders</ion-label></ion-tab-button>
+      <ion-tab-button tab="profile" [routerLink]="['/tabs/profile']"><ion-label>Profile</ion-label></ion-tab-button>
+      @if(showAdmin){<ion-tab-button tab="admin" [routerLink]="['/tabs/admin']"><ion-label>Admin</ion-label></ion-tab-button>}
     </ion-tab-bar>
   </ion-tabs>
   `
